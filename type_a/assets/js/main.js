c18625d2af5c4b351d9093e71e4e63e153533285
uforgot <uforgot@designfever.com>
2018-03-27 12:00:15 +0900

    문구 수정 / type-a 등장 모션 수정

--- type_a/assets/js/main.js
+++ type_a/assets/js/main.js
@@ -246,14 +246,14 @@ var main = (function($) {
         $('.popup-con').load('popup.html .popup',subLoadComplete);
 
         setTimeout(show1, 500);
-        setTimeout(show2, 1000);
-        setTimeout(show3, 1400);
+        setTimeout(show2, 900);
+        setTimeout(show3, 1200);
     };
 
     var show1 = function() {
         // $('.card-float img').addClass('onShow');
 
-        $('.card-float img').animate({ 'opacity': 1, 'transform':'scale(1)' }, 500, 'easeOutBounce');
+        $('.card-float img').animate({ 'opacity': 1, 'transform':'scale(1)' }, 900, 'easeInOutElastic');
     };
 
     var show2 = function() {
